0298bc4909890a10d860f6d91334635f0605454f
颜丙体 <yanbt@weipaitang.com>
2021-03-04 11:03:40 +0800

    fix: preg language

--- dmp3.go
+++ dmp3.go
@@ -16,16 +16,17 @@ func main() {
 
 	// 循环处理文件
 	_ = supports.ReadFile(originFile, func(s string) {
-		// userinfo_id,device_id,identity,os,device_type,dt
-		sli := strings.Split(s, ",")
-		if sli[4] == "imei" {
-			_, _ = imei.WriteString(sli[1] + "\n")
+		// userinfo_id	os	imei	oaid	android_id	idfa	idfv	caid
+		sli := strings.Split(s, "	")
+
+		if len(sli) >= 3 && supports.Mimei(sli[2]) {
+			_, _ = imei.WriteString(sli[2] + "\n")
 		}
-		if sli[4] == "idfa" {
-			_, _ = idfa.WriteString(sli[1] + "\n")
+		if len(sli) >= 4 && supports.Moaid(sli[3]) {
+			_, _ = oaid.WriteString(sli[3] + "\n")
 		}
-		if sli[4] == "oaid" {
-			_, _ = oaid.WriteString(sli[1] + "\n")
+		if len(sli) >= 6 && supports.Midfa(sli[5]) {
+			_, _ = idfa.WriteString(sli[5] + "\n")
 		}
 	})
 }
